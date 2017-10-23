531a5ebf0050ac5c16903b56c632d226702726cf
Jamie Prentice <2858046+JamiePrentice@users.noreply.github.com>
2017-10-23 23:17:07 +0100

    Request - Working but shitty example.

---
+++ Models/Request.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Models.Request
+{
+    public class Request
+    {
+        public async void GetData()
+        {
+            //We will make a GET request to a really cool website...
+
+            string baseUrl = "http://mwolfhoffman.com";
+            //The 'using' will help to prevent memory leaks.
+            //Create a new instance of HttpClient
+            HttpClient client = new HttpClient();
+
+            //Setting up the response...
+
+            HttpResponseMessage res = await client.GetAsync(baseUrl);
+            HttpContent content = res.Content;
+            {
+                string data = await content.ReadAsStringAsync();
+                if (data != null)
+                {
+                    Console.WriteLine(data);
+                }
+            }
+        }
+
+        public Request()
+        {
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Models.Request;
 using Swashbuckle.AspNetCore.Swagger;
 
 namespace Comments
@@ -34,6 +35,9 @@ namespace Comments
                     License = new License { Name = "Use under LICX", Url = "https://example.com/license" }
                 });
             });
+
+            var request = new Request();
+            request.GetData();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
