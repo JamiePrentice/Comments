e6d5b4add3cf1fb04c547b39690cc69b4c720891
Jamie Prentice <2858046+JamiePrentice@users.noreply.github.com>
2017-10-26 23:28:12 +0100

    Removed old method implementation.

--- Models/Request.cs
+++ Models/Request.cs
@@ -6,28 +6,6 @@ namespace Models.Request
 {
     public class Request
     {
-        public async void GetData()
-        {
-            //We will make a GET request to a really cool website...
-
-            string baseUrl = "http://mwolfhoffman.com";
-            //The 'using' will help to prevent memory leaks.
-            //Create a new instance of HttpClient
-            HttpClient client = new HttpClient();
-
-            //Setting up the response...
-
-            HttpResponseMessage res = await client.GetAsync(baseUrl);
-            HttpContent content = res.Content;
-            {
-                string data = await content.ReadAsStringAsync();
-                if (data != null)
-                {
-                    Console.WriteLine(data);
-                }
-            }
-        }
-
         public async Task<string> Send(string url)
         {
             HttpClient client = new HttpClient();
