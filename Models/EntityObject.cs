7bf4607703846f89df0fc37ba9369a09e326724c
Jamie Prentice <jamie.prentice@amorgroup.com>
2017-12-08 09:26:11 +0000

    WIP Bullshit

--- Models/EntityObject.cs
+++ Models/EntityObject.cs
@@ -1,23 +1,26 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Comments.Models
 {
-    public abstract class EntityObject
-    {
-	    #region Properties
+	public abstract class EntityObject
+	{
+		#region Properties
 
 		public Guid Id { get; set; }
+
 		public DateTime CreatedTime { get; set; }
 		public DateTime? UpdatedTime { get; set; }
 		public DateTime? DeletedTime { get; set; }
 
 		public bool IsDeleted { get; set; }
 
-		#endregion
+		#endregion Properties
 
 		public void Delete()
-	    {
-		    IsDeleted = true;
-	    }
+		{
+			IsDeleted = true;
+			DeletedTime = DateTime.Now;
+		}
 	}
 }
