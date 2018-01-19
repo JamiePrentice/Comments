526874f4f7adf97b8d760a15fb0b8dbec5d32404
Jamie Prentice <JamiePrentice@users.noreply.github.com>
2018-01-19 21:32:04 +0000

    Consistency is key - formatting and consistant quote marks

--- UI/comments.js
+++ UI/comments.js
@@ -1,18 +1,20 @@
 generateForm();
 
 function postComment() {
-    alert('Well hello there');
+    alert("Well hello there");
     var comment = {
-        "text": document.getElementById('comments-comment').value,
-        "username": document.getElementById('comments-comment').value,
+        "text": document.getElementById("comments-comment").value,
+        "username": document.getElementById("comments-comment").value,
         "ipAddress": "string",
         "domain": "string",
         "url": "string",
         "parentCommentId": 0,
-    }
+    };
+
+    postRequest("localhost:port/api/comment", comment);
 }
 
-function postRequest(url, data){
+function postRequest(url, data) {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-type", "application/json");
@@ -26,27 +28,27 @@ function postRequest(url, data){
 
 function generateForm() {
 
-    var form = document.getElementById('comments');
-    form.appendChild(document.createTextNode('Comment:'));
-    form.appendChild(document.createElement('br'))
+    var form = document.getElementById("comments");
+    form.appendChild(document.createTextNode("Comment:"));
+    form.appendChild(document.createElement("br"));
 
-    var comment = form.appendChild(document.createElement('textarea'));
-    comment.id = 'comments-comment'
-    comment.name = 'comment';
-    comment.type = 'text';
-    comment.rows = '4';
-    comment.cols = '50'
+    var comment = form.appendChild(document.createElement("textarea"));
+    comment.id = "comments-comment";
+    comment.name = "comment";
+    comment.type = "text";
+    comment.rows = "4";
+    comment.cols = "50";
 
-    form.appendChild(document.createElement('br'))
+    form.appendChild(document.createElement("br"));
 
-    form.appendChild(document.createTextNode('Name:'));
+    form.appendChild(document.createTextNode("Name:"));
 
-    var name = form.appendChild(document.createElement('input'));
-    name.name = 'name';
-    name.type = 'text';
+    var name = form.appendChild(document.createElement("input"));
+    name.name = "name";
+    name.type = "text";
 
-    var button = form.appendChild(document.createElement('button'));
-    button.innerHTML = 'Submit';
+    var button = form.appendChild(document.createElement("button"));
+    button.innerHTML = "Submit";
     button.onclick = function () {
         postComment();
     };
