6edf6b44e5b4a3cdc49e49dccc7b7feb1b9ca501
Jamie Prentice <jamie.prentice@amorgroup.com>
2018-01-19 15:21:19 +0000

    Javascript postRequest

--- UI/comments.js
+++ UI/comments.js
@@ -1,23 +1,43 @@
 generateForm();
 
-function submitForm() {
-    alert("Well hello there");
-    // https://developer.mozilla.org/en-US/docs/Web/API/FormData/Using_FormData_Objects
+function postComment() {
+    alert('Well hello there');
+    var comment = {
+        "text": document.getElementById('comments-comment').value,
+        "username": document.getElementById('comments-comment').value,
+        "ipAddress": "string",
+        "domain": "string",
+        "url": "string",
+        "parentCommentId": 0,
+    }
+}
+
+function postRequest(url, data){
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var json = JSON.parse(xhr.responseText);
+        }
+    };
+    xhr.send(data);
 }
 
 function generateForm() {
 
     var form = document.getElementById('comments');
     form.appendChild(document.createTextNode('Comment:'));
-    form.appendChild(document.createElement("br"))
+    form.appendChild(document.createElement('br'))
 
     var comment = form.appendChild(document.createElement('textarea'));
+    comment.id = 'comments-comment'
     comment.name = 'comment';
     comment.type = 'text';
-    comment.rows = "4";
-    comment.cols = "50"
+    comment.rows = '4';
+    comment.cols = '50'
 
-    form.appendChild(document.createElement("br"))
+    form.appendChild(document.createElement('br'))
 
     form.appendChild(document.createTextNode('Name:'));
 
@@ -28,6 +48,6 @@ function generateForm() {
     var button = form.appendChild(document.createElement('button'));
     button.innerHTML = 'Submit';
     button.onclick = function () {
-        submitForm();
+        postComment();
     };
 }
