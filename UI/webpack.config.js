e13371809f08d3a27fcf9ea975e5e611815b59fc
Jamie Prentice <jamie.prentice@amorgroup.com>
2018-01-26 15:55:42 +0000

    Webpack CSS attempts

--- UI/webpack.config.js
+++ UI/webpack.config.js
@@ -1,6 +1,6 @@
-var webpack = require('webpack');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require('webpack');
 const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   watch: true,
@@ -8,6 +8,29 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.min.js'
+  },
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [{
+        loader: "style-loader"
+      }, {
+        loader: "css-loader",
+        options: {
+          alias: {
+            "../fonts/bootstrap": "bootstrap-sass/assets/fonts/bootstrap"
+          }
+        }
+      }, {
+        loader: "sass-loader",
+        options: {
+          includePaths: [
+            path.resolve("./node_modules/bootstrap-sass/assets/stylesheets")
+          ]
+        }
+      }]
+    }
+  ]
   },
   plugins: [
     new UglifyJSPlugin()
