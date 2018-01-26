324e4caa6aba897dcad62665de4eb30c2b4931c3
Jamie Prentice <jamie.prentice@amorgroup.com>
2018-01-26 13:01:42 +0000

    WebPack - Watch

--- UI/webpack.config.js
+++ UI/webpack.config.js
@@ -3,15 +3,13 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+  watch: true,
   entry: './comments.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.min.js'
   },
   plugins: [
-    new UglifyJSPlugin({
-      // compress: { warnings: false },
-      // minimize: true
-    })
+    new UglifyJSPlugin()
   ]
 };
